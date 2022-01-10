99ba2b5cc23875151f70d7f01c3bf4cf217faa0f
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-10 20:48:03 +0100

    create database connection

---
+++ database/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE advert (
+    id bigint
+    title varchar(80),
+    description varchar(80)
+);
+
+INSERT INTO advert (id, title, description)
+VALUES (1, "title", "description")
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 // Import the express in typescript file
-import express from 'express';
+import express from "express";
+// postgress pool
+import pool from "./database/config";
 //Environment variables
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 // Initialize the express engine
@@ -10,26 +12,36 @@ const app: express.Application = express();
 // Take a port 3000 for running server.
 const port: string | number = process.env.PORT || 3000;
 
-const adsRouter: express.Router = require('./routes/ads.ts');
+const adsRouter: express.Router = require("./routes/ads.ts");
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 //Routers
 
-app.use('/ads', adsRouter);
+app.use("/ads", adsRouter);
 
 // Handling '/' Request
-app.get('/', (req, res): void => {
-	res.json({
-		message: 'Successfully accessed the endpoint',
-		success: true,
-		payload: {},
-	});
+app.get("/", (req, res): void => {
+  res.json({
+    message: "Successfully accessed the endpoint",
+    success: true,
+    payload: {},
+  });
+});
+// get ads from our postgress db
+app.get("/adverts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM advert");
+    const results = { results: result ? result.rows : null };
+    res.send(results);
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 // Server setup
 app.listen(port, (): void => {
-	console.log(`TypeScript with Express http://localhost:${port}/`);
+  console.log(`TypeScript with Express http://localhost:${port}/`);
 });
