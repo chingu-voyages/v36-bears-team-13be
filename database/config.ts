b8aaa0441a655fb27602b405a2661db26dbb0081
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-11 20:45:31 +0100

    if production ssl true else false

--- database/config.ts
+++ database/config.ts
@@ -7,7 +7,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 console.log("asd", process.env.DB_USER);
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 export default pool;
