234a9f7f806b21235ef66445a3bf9eb102996ffb
corshi.dan <corshi.dan@gmail.com>
2022-01-11 19:45:02 +0000

    separated database logic from routes

---
+++ models/adverts.ts
@@ -0,0 +1,12 @@
+// postgress pool
+const query = require('../database/config');
+
+export default async function getAllAdverts() {
+	try {
+		const sqlString: string = 'SELECT * FROM adverts;';
+		const adverts = await query(sqlString);
+		return adverts.rows[0];
+	} catch (error) {
+		console.error(error);
+	}
+}
