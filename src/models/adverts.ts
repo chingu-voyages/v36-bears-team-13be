74c2d2cc451a4909381d89e622dde5d455e698c5
corshi.dan <corshi.dan@gmail.com>
2022-01-20 15:28:52 +0000

    ADD: endpoint for all adverts | endpoint to add a new advert

--- src/models/adverts.ts
+++ src/models/adverts.ts
@@ -1,14 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+import { Advert } from '@src/utils/databaseTypes';
+
 const { query } = require('../database/config');
 
-type IAdvert = {
-  id: number;
-  title: string;
-  description: string;
-};
-
-export default async function getAllAdverts(): Promise<unknown> {
+export async function getAllAdverts(): Promise<Advert> {
   const sqlString = 'SELECT * FROM advert;';
   const adverts = await query(sqlString);
-  return adverts.rows[0];
+  return adverts.rows;
 }
+export async function addNewAdvert(advert: Advert): Promise<Array<Advert> | null> {
+  const sqlString =
+    'INSERT INTO advert (title, description,price,area,availability,experience) VALUES($1,$2,$3,$4,$5,$6) RETURNING id;';
+  try {
+    const response = await query(sqlString, [
+      advert.title,
+      advert.description,
+      advert.price,
+      advert.area,
+      advert.availability,
+      advert.experience,
+    ]);
+    return response.rows[0];
+  } catch (error) {
+    console.error(error);
+  }
+  return null;
+}
+
+// TODO: functions to UPDATE / DELETE adverts
